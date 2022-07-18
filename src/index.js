bf69202f952e6aabaa7997cb0927c9f448e1a88c
Minh Tuan Hoang <hmtuan9a@gmail.com>
2022-07-18 23:08:16 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import GlobalStyles from "./components/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <GlobalStyles>
     <App />
--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import classNames from 'classnames/bind'
 import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import NewsItem from '../../components/NewsItem/NewsItem';
 import NewsModal from '../../components/NewsModal/NewsModal';
@@ -13,9 +14,10 @@ function News() {
   const [newsItems, setNewsItems] = useState([]);
   const [newsId, setNewsId] = useState(null)
   const [currentNews, setCurrentNews] = useState(null);
+  const [page, setPage] = useState(0)
 
   useEffect(() => {
-    const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aGFva2lldXZhbkBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9VU0VSLFJPTEVfQURNSU4iLCJ1aWQiOjQ1LCJleHAiOjE2NTgxMjI4ODF9.z3Mf29hC8EPd-VZeendLnKTqW_GIUnYHi-dVdQDReJYxCVFiTXyJeH08uZs4vHRkYziUaDrA0WN9oo0DKzr5qA"
+    const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aGFva2lldXZhbkBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9VU0VSLFJPTEVfQURNSU4iLCJ1aWQiOjQ1LCJleHAiOjE2NTgyOTkwMTl9.z6YKLl-YoyVL8NXCszluoV7s4JuWctCSDjvyFLDcJU9TgVfhhGMZB68rnoaqFBw1MVqWjVDTck5wFz5qzFORRA"
 
     axios({
       method: "get",
@@ -31,7 +33,7 @@ function News() {
   }, [])
 
   useEffect(() => {
-    const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aGFva2lldXZhbkBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9VU0VSLFJPTEVfQURNSU4iLCJ1aWQiOjQ1LCJleHAiOjE2NTgxMjI4ODF9.z3Mf29hC8EPd-VZeendLnKTqW_GIUnYHi-dVdQDReJYxCVFiTXyJeH08uZs4vHRkYziUaDrA0WN9oo0DKzr5qA"
+    const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aGFva2lldXZhbkBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9VU0VSLFJPTEVfQURNSU4iLCJ1aWQiOjQ1LCJleHAiOjE2NTgyOTkwMTl9.z6YKLl-YoyVL8NXCszluoV7s4JuWctCSDjvyFLDcJU9TgVfhhGMZB68rnoaqFBw1MVqWjVDTck5wFz5qzFORRA"
 
     if (newsId) {
       axios({
@@ -58,17 +60,44 @@ function News() {
     setIsModalVisible(false);
   };
 
+  const fetMoreData = () => {
+    setPage(page + 1)
+    console.log(page)
+
+    const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aGFva2lldXZhbkBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9VU0VSLFJPTEVfQURNSU4iLCJ1aWQiOjQ1LCJleHAiOjE2NTgyOTkwMTl9.z6YKLl-YoyVL8NXCszluoV7s4JuWctCSDjvyFLDcJU9TgVfhhGMZB68rnoaqFBw1MVqWjVDTck5wFz5qzFORRA"
+
+    axios({
+      method: "get",
+      url: `https://api.simplize.dev/api/company/news-event/list?page=${page}&size=15`,
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    })
+      .then(res => {
+        console.log(res.data);
+        const data = res.data.data
+        setNewsItems([...newsItems, ...data]);
+      })
+  }
+
   return (
     <div className={cx('wrapper')}>
-      {
-        newsItems.map((newsItem, index) => (
-          <NewsItem
-            newsItem={newsItem}
-            onClick={() => showModal(newsItem.id)}
-            key={index}
-          />
-        ))
-      }
+      <InfiniteScroll
+        dataLength={newsItems.length}
+        next={fetMoreData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        {
+          newsItems.map((newsItem, index) => (
+            <NewsItem
+              newsItem={newsItem}
+              onClick={() => showModal(newsItem.id)}
+              key={index}
+            />
+          ))
+        }
+      </InfiniteScroll>
 
       {currentNews && <NewsModal
         isModalVisible={isModalVisible}
